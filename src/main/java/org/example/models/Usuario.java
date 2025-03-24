d4e06544a839f52cbf848e4cbbe47dec4f3b2725
AlejandroJ <157817997+JandroPlayer@users.noreply.github.com>
2025-03-24 15:37:15 +0100

    Reducir Usuario.java

--- src/main/java/org/example/models/Usuario.java
+++ src/main/java/org/example/models/Usuario.java
@@ -1,11 +1,11 @@
 package org.example.models;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-@Getter
-@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 @Entity
 @Table(name = "usuarios")
 public class Usuario {
